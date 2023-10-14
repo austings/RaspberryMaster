b4b172792a376ff33445d23a077dba35a4e05258
Austin <nyrochesterus@gmail.com>
2023-10-14 19:25:34 +0200

    LED blinking on PICO

---
+++ blink/LEDBlink/build/CMakeFiles/LEDBlink.dir/home/austin/pico/pico-sdk/lib/tinyusb/src/class/msc/msc_device.c.obj.d
@@ -0,0 +1,31 @@
+CMakeFiles/LEDBlink.dir/home/austin/pico/pico-sdk/lib/tinyusb/src/class/msc/msc_device.c.obj: \
+ /home/austin/pico/pico-sdk/lib/tinyusb/src/class/msc/msc_device.c \
+ /home/austin/pico/pico-sdk/lib/tinyusb/src/tusb_option.h \
+ /home/austin/pico/pico-sdk/lib/tinyusb/src/common/tusb_compiler.h \
+ /home/austin/pico/pico-sdk/src/rp2_common/pico_stdio_usb/include/tusb_config.h \
+ /home/austin/pico/pico-sdk/src/rp2_common/pico_stdio_usb/include/pico/stdio_usb.h \
+ /home/austin/pico/pico-sdk/src/rp2_common/pico_stdio/include/pico/stdio.h \
+ /home/austin/pico/pico-sdk/src/common/pico_base/include/pico.h \
+ /home/austin/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/austin/pico/pico-sdk/src/common/pico_base/include/pico/assert.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /usr/include/newlib/assert.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /home/austin/pico/blink/LEDBlink/build/generated/pico_base/pico/version.h \
+ /home/austin/pico/pico-sdk/src/common/pico_base/include/pico/config.h \
+ /home/austin/pico/blink/LEDBlink/build/generated/pico_base/pico/config_autogen.h \
+ /home/austin/pico/pico-sdk/src/boards/include/boards/pico.h \
+ /home/austin/pico/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ /home/austin/pico/pico-sdk/src/rp2_common/pico_platform/include/pico/platform.h \
+ /home/austin/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ /home/austin/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
+ /home/austin/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h \
+ /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /home/austin/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/austin/pico/pico-sdk/src/common/pico_base/include/pico/error.h \
+ /home/austin/pico/pico-sdk/lib/tinyusb/src/common/tusb_mcu.h
---
+++ blink/LEDBlink/build/CMakeFiles/LEDBlink.dir/home/austin/pico/pico-sdk/src/rp2_common/pico_stdlib/stdlib.c.obj.d
@@ -0,0 +1,54 @@
+CMakeFiles/LEDBlink.dir/home/austin/pico/pico-sdk/src/rp2_common/pico_stdlib/stdlib.c.obj: \
+ /home/austin/pico/pico-sdk/src/rp2_common/pico_stdlib/stdlib.c \
+ /home/austin/pico/pico-sdk/src/common/pico_stdlib/include/pico/stdlib.h \
+ /home/austin/pico/pico-sdk/src/common/pico_base/include/pico.h \
+ /home/austin/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/austin/pico/pico-sdk/src/common/pico_base/include/pico/assert.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /usr/include/newlib/assert.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /home/austin/pico/blink/LEDBlink/build/generated/pico_base/pico/version.h \
+ /home/austin/pico/pico-sdk/src/common/pico_base/include/pico/config.h \
+ /home/austin/pico/blink/LEDBlink/build/generated/pico_base/pico/config_autogen.h \
+ /home/austin/pico/pico-sdk/src/boards/include/boards/pico.h \
+ /home/austin/pico/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ /home/austin/pico/pico-sdk/src/rp2_common/pico_platform/include/pico/platform.h \
+ /home/austin/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ /home/austin/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
+ /home/austin/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h \
+ /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /home/austin/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/austin/pico/pico-sdk/src/common/pico_base/include/pico/error.h \
+ /home/austin/pico/pico-sdk/src/rp2_common/pico_stdio/include/pico/stdio.h \
+ /home/austin/pico/pico-sdk/src/common/pico_time/include/pico/time.h \
+ /home/austin/pico/pico-sdk/src/rp2_common/hardware_timer/include/hardware/timer.h \
+ /home/austin/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/timer.h \
+ /home/austin/pico/pico-sdk/src/rp2_common/hardware_base/include/hardware/address_mapped.h \
+ /home/austin/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/timer.h \
+ /home/austin/pico/pico-sdk/src/rp2_common/hardware_gpio/include/hardware/gpio.h \
+ /home/austin/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/sio.h \
+ /home/austin/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/interp.h \
+ /home/austin/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/padsbank0.h \
+ /home/austin/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/pads_bank0.h \
+ /home/austin/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/iobank0.h \
+ /home/austin/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/io_bank0.h \
+ /home/austin/pico/pico-sdk/src/rp2_common/hardware_irq/include/hardware/irq.h \
+ /home/austin/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/intctrl.h \
+ /home/austin/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/m0plus.h \
+ /home/austin/pico/pico-sdk/src/rp2_common/hardware_uart/include/hardware/uart.h \
+ /home/austin/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/uart.h \
+ /home/austin/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/uart.h \
+ /home/austin/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/dreq.h \
+ /home/austin/pico/pico-sdk/src/rp2_common/pico_stdio_uart/include/pico/stdio_uart.h \
+ /home/austin/pico/pico-sdk/src/rp2_common/pico_stdio_usb/include/pico/stdio_usb.h \
+ /home/austin/pico/pico-sdk/src/rp2_common/hardware_pll/include/hardware/pll.h \
+ /home/austin/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/pll.h \
+ /home/austin/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/pll.h \
+ /home/austin/pico/pico-sdk/src/rp2_common/hardware_clocks/include/hardware/clocks.h \
+ /home/austin/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/clocks.h \
+ /home/austin/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/clocks.h
